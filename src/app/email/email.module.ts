474268db7d9f59c2ae71356e6bfc9a17255cfb01
Mate Smajda <mate.smajda@accretivetg.com>
2024-08-21 22:21:11 +0200

    email module cleanup

--- src/app/email/email.module.ts
+++ src/app/email/email.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Email, EmailSchema } from './email.schema';
 import { EmailService } from './email.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Email.name, schema: EmailSchema }])],
-  controllers: [],
   providers: [EmailService],
   exports: [EmailService],
 })
--- src/app/email/email.service.ts
+++ src/app/email/email.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Email } from './email.schema';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import * as mail from '@sendgrid/mail';
 import { ConfigService } from '@nestjs/config';
@@ -20,8 +17,6 @@ export class EmailService {
     configService: ConfigService,
     @InjectPinoLogger(EmailService.name)
     private readonly logger: PinoLogger,
-    @InjectModel(Email.name)
-    private emailModel: Model<Email>,
   ) {
     this.sendGridApiKey = configService.get<string>('SENDGRID_API_KEY');
     this.verificationTemplateId = configService.get<string>('EMAIL_VERIFICATION_TEMPLATE_ID');
