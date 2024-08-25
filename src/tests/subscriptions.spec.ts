81db6a86ee96841aba5cc105979029f27317d7ff
Mate Smajda <mate.smajda@accretivetg.com>
2024-08-25 12:29:29 +0200

    cleanup test

--- src/tests/subscriptions.spec.ts
+++ src/tests/subscriptions.spec.ts
@@ -18,17 +18,6 @@ describe('getDepthForRequestedStorage', () => {
     [90, 8_000_000, 41, 90 * 414720000, '8207810.32'],
   ])('should return the smallest possible depth', async (days, gbs, expectedDepth, expectedAmount, expectedPrice) => {
     const result = calculateDepthAndAmount(days, gbs);
-
-    console.log(`Using ${result.amount}`);
-    for (let i = 1; i < 9_000_000; i *= 2) {
-      const result = calculateDepthAndAmount(30, i);
-      const usd = Number(result.bzzPrice) * 0.3;
-      const huf = usd * 365;
-      console.log(
-        `Using depth: ${result.depth} for storing \t GBs:${i.toFixed(0).padStart(7)} \t BZZ:${result.bzzPrice.padStart(9)}\t USD ${usd.toFixed(1).padStart(9)} \t HUF ${Math.ceil(huf).toFixed(0).padStart(9)}`,
-      );
-    }
-
     expect(result.depth).toBe(expectedDepth);
     expect(result.amount).toBe(expectedAmount);
     expect(result.bzzPrice).toBe(expectedPrice);
